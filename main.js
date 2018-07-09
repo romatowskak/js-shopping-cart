3a39b6363e97a100a0276231c630aad38f4ff07f
Adrian Bienias <adrianbienias@gmail.com>
2018-07-09 12:56:14 +0200

    Lecture 04

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
       image: productDOM.querySelector('.product__image').getAttribute('src'),
       name: productDOM.querySelector('.product__name').innerText,
       price: productDOM.querySelector('.product__price').innerText,
+      quantity: 1,
     };
 
     const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
@@ -19,13 +20,30 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
       cartDOM.insertAdjacentHTML('beforeend', `
         <div class="cart__item">
           <img class="cart__item__image" src="${product.image}" alt="${product.name}">
-          <h3 class="class__item__name">${product.name}</h3>
-          <h3 class="class__item__price">${product.price}</h3>
+          <h3 class="cart__item__name">${product.name}</h3>
+          <h3 class="cart__item__price">${product.price}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECRESE_ITEM">&minus;</button>
+          <h3 class="cart__item__quantity">${product.quantity}</h3>
+          <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
         </div>
       `);
 
       cart.push(product);
       addToCartButtonDOM.innerText = 'In Cart';
+
+      const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+      cartItemsDOM.forEach(cartItemDOM => {
+        if (cartItemDOM.querySelector('.cart__item__name').innerText === product.name) {
+          cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
+            cart.forEach(cartItem => {
+              if (cartItem.name === product.name) {
+                cartItemDOM.querySelector('.cart__item__quantity').innerText = ++cartItem.quantity;
+              }
+            });
+          });
+        }
+      });
     }
   });
 });
